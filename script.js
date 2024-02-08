3312b0ee7779dd07bfa31e060dc94165919f5a57
Hemang <hemangsharma1001@gmail.com>
2024-02-08 16:51:20 +0530

    bug fix

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const inputBox = document.getElementById("input-box") ;
 const listContainer = document.getElementById("list-container");
 
 function addTask(){
-    if(inputBox.value === '')
+    if(inputBox.value.trim() === '')
     {
         alert("you must write something");
     }
